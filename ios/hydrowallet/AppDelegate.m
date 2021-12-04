1a11305eb63bb21cb737b1bee5856f27b5c49954
Gallbers <gagzu.ali@gmail.com>
2021-12-04 17:00:50 -0300

    update AppDelegate

--- ios/hydrowallet/AppDelegate.m
+++ ios/hydrowallet/AppDelegate.m
@@ -3,6 +3,7 @@
 #import <React/RCTBridge.h>
 #import <React/RCTBundleURLProvider.h>
 #import <React/RCTRootView.h>
+#import <React/RCTLinkingManager.h>
 #import <React/RCTConvert.h>
 
 #ifdef FB_SONARKIT_ENABLED
@@ -52,13 +53,31 @@ static void InitializeFlipper(UIApplication *application) {
   return YES;
 }
 
+- (NSArray<id<RCTBridgeModule>> *)extraModulesForBridge:(RCTBridge *)bridge
+{
+  // If you'd like to export some custom RCTBridgeModules that are not Expo modules, add them here!
+  return @[];
+}
+
 - (NSURL *)sourceURLForBridge:(RCTBridge *)bridge
 {
-#if DEBUG
-  return [[RCTBundleURLProvider sharedSettings] jsBundleURLForBundleRoot:@"index" fallbackResource:nil];
-#else
-  return [[NSBundle mainBundle] URLForResource:@"main" withExtension:@"jsbundle"];
-#endif
+  #if DEBUG
+    return [[RCTBundleURLProvider sharedSettings] jsBundleURLForBundleRoot:@"index" fallbackResource:nil];
+  #else
+    return [[NSBundle mainBundle] URLForResource:@"main" withExtension:@"jsbundle"];
+  #endif
 }
+
+ // Linking API
+ - (BOOL)application:(UIApplication *)application openURL:(NSURL *)url options:(NSDictionary<UIApplicationOpenURLOptionsKey,id> *)options {
+   return [RCTLinkingManager application:application openURL:url options:options];
+ }
+
+ // Universal Links
+ - (BOOL)application:(UIApplication *)application continueUserActivity:(nonnull NSUserActivity *)userActivity restorationHandler:(nonnull void (^) (NSArray<id<UIUserActivityRestoring>> * _Nullable))restorationHandler {
+   return [RCTLinkingManager application:application
+                    continueUserActivity:userActivity
+                      restorationHandler:restorationHandler];
+ }
 
 @end
