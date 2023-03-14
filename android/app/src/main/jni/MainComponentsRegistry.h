a0f074686c5685a9cb68e225da654f191a7caa0b
Pab Mac <pabmac@msn.com>
2023-03-13 22:55:55 -0300

    Fix bundle

--- android/app/src/main/jni/MainComponentsRegistry.h
+++ android/app/src/main/jni/MainComponentsRegistry.h
@@ -13,7 +13,7 @@ class MainComponentsRegistry
  public:
   // Adapt it to the package you used for your Java class.
   constexpr static auto kJavaDescriptor =
-      "Lcom/aegir/wallet/newarchitecture/components/MainComponentsRegistry;";
+      "Lcom/aegirwallet/newarchitecture/components/MainComponentsRegistry;";
 
   static void registerNatives();
 
